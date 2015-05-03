86fc4af1a51e3092fa47cfaa81cde9d0bb4e8c53
marco <oliveira.marco.andre@gmail.com>
2015-05-02 23:28:29 -0500

    Added scss files for materialize; changed default colors

--- gruntfile.js
+++ gruntfile.js
@@ -47,7 +47,7 @@ module.exports = function(grunt) {
         tasks: ['copy']
       }, //script
       sass: {
-        files: ['sass/*.scss'],
+        files: ['sass/*.scss', 'sass/components/*.scss'],
         tasks: ['compass:dev']
       },
       html: {
